6030ae5284dac0d8461a1daa7e8753c468f2d302
mt1006 <damianm916@gmail.com>
2024-07-15 20:15:37 +0200

    Version for NeoForge

--- src/main/java/com/mt1006/nbt_copy/NBTcopy.java
+++ src/main/java/com/mt1006/nbt_copy/NBTcopy.java
@@ -2,7 +2,7 @@ package com.mt1006.nbt_copy;
 
 import com.mojang.logging.LogUtils;
 import net.minecraft.network.chat.*;
-import net.minecraftforge.fml.common.Mod;
+import net.neoforged.fml.common.Mod;
 import org.slf4j.Logger;
 
 @Mod(NBTcopy.MOD_ID)
@@ -11,7 +11,7 @@ public class NBTcopy
 	public static final String MOD_ID = "nbt_copy";
 	public static final String VERSION = "1.0.1";
 	public static final String FOR_VERSION = "1.21";
-	public static final String FOR_LOADER = "Forge";
+	public static final String FOR_LOADER = "NeoForge";
 	public static final Logger LOGGER = LogUtils.getLogger();
 	public static boolean skipButton = false;
 
